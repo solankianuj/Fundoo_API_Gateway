17d8c1c2395f1ac19533d4cdd67e6ddab45fd446
solanki <anujaj2222.gmail.com>
2022-09-16 22:24:45 +0530

    Fundoo API gateway

---
+++ src/main/java/com/example/fundoonoteapigateway/FundooNoteApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundoonoteapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNoteApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundooNoteApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fundoonoteapigateway/FundooNoteApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fundoonoteapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooNoteApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
